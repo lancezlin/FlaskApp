e40852763b163a983f811045a4bede584e5cedeb
lancezlin <linzhengzhong@icloud.com>
2015-09-07 23:42:16 -0700

    call stored procedure when hit set up

--- app.py
+++ app.py
@@ -1,29 +1,58 @@
-from flask import Flask, render_template, request, json
-#import json from flask
+from flask import Flask, render_template, json, request
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
+mysql = MySQL()
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-	return render_template('index.html')
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Pi=3.1415926'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
+
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
 
-@app.route('/signUp',  methods=['POST'])
+
+@app.route('/signUp',methods=['POST','GET'])
 def signUp():
-	# read the posted values from the UI
-	_name = request.form['inputName']
-	_email = request.form['inputEmail']
-	_password = request.form['inputPassword']
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
 
-	# validate the received values
-	if _name and _email and _password:
-		return json.dumps({'html':'<span>All fields good!</span>'})
-	else:
-		return json.dumps({'html':'<span>Enter the required fields</span>'})
+        # validate the received values
+        if _name and _email and _password:
+            
+            # All Good, let's call MySQL
+            
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
 
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5002)
