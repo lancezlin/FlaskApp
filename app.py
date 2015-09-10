d1c39974c56cd5988fddac7ba86a6217bb260926
lancezlin <linzhengzhong@icloud.com>
2015-09-09 22:58:44 -0700

    add validate user login method

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ def showSignUp():
 def showSignin():
     return render_template('signin.html')
 
+@app.route('/userHome')
+def userHome():
+    return render_template('userHome.html')
 
 @app.route('/signUp',methods=['POST','GET'])
 def signUp():
@@ -57,6 +60,34 @@ def signUp():
     finally:
         cursor.close() 
         conn.close()
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+ 
+        # connect to mysql
+ 
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+  
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+  
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
 
 if __name__ == "__main__":
     app.run()
