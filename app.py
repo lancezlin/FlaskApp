7275405bb42adb4be6b640b37505d8da2d507ff1
Lance Lin <linzhengzhong@icloud.com>
2015-09-11 10:27:29 -0700

    Update app.py
    import jsonify

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request, redirect, session
+from flask import Flask, render_template, json, request, redirect, session, jsonify
 from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
